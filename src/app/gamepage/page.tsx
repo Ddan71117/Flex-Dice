d3ab206d0712750bd91a32786905ebf7e7f3d950
mvoidets <mvoidets@yahoo.com>
2025-01-30 19:54:49 -0700

    wrapped return in layout

--- src/app/gamepage/page.tsx
+++ src/app/gamepage/page.tsx
@@ -4,11 +4,12 @@ import DiceCluster from '../components/DiceCluster';
 import ChatBox from '../components/ChatBox';
 import Stats from '../components/Stats';
 import MainDropdown from '../components/MainDropdown';
+import Layout from "../layout";
 
 
 const GamePage: React.FC = () => {
   const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null);
- 
+
 
   useEffect(() => {
     // Retrieve avatar from localStorage on page load
@@ -18,13 +19,15 @@ const GamePage: React.FC = () => {
     }
   }, []);
 
- 
+
   return (
     <div className="relative min-h-screen bg-transparent">
-     {/* game logic here */ }
-   <ChatBox />
-   <Stats />
-   <MainDropdown />
+      {/* game logic here */}
+      <Layout>
+        <MainDropdown />
+      </Layout>
+      <ChatBox />
+      <Stats />
     </div>
   );
 };
