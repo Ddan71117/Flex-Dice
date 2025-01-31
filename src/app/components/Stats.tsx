bde9b4a522087266405a4b231218ec67a85371b7
mvoidets <mvoidets@yahoo.com>
2025-01-30 19:24:05 -0700

    made modal for stats and chat

--- src/app/components/Stats.tsx
+++ src/app/components/Stats.tsx
@@ -7,7 +7,7 @@ export default function Stats() {
   const [losses, setLosses] = useState(0);
   const [gamesPlayed, setGamesPlayed] = useState(0);
   const [userName, setUserName] = useState("");
-  
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Simulating game stats for demonstration
   useEffect(() => {
@@ -22,21 +22,52 @@ export default function Stats() {
 
   }, []);
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className="fixed bottom-4 right-4 w-60 max-w-3xl mx-auto p-4 bg-white border rounded-lg shadow-lg">
-      <h3 className="text-2xl  font-bold text-gray-900">{userName} Stats</h3>
-      <div className="flex justify-between text-black text-sm mb-2">
-        <span>Winnings:</span>
-        <span>{winnings}</span>
-      </div>
-      <div className="flex justify-between text-black text-sm mb-2">
-        <span>Losses:</span>
-        <span>{losses}</span>
-      </div>
-      <div className="flex justify-between text-black text-sm mb-2">
-        <span>Games Played:</span>
-        <span>{gamesPlayed}</span>
-      </div>
+    <div>
+      <button
+        onClick={openModal}
+        className="fixed bottom-4 right-4 h-14 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+      >
+        View Your Stats
+      </button>
+
+
+      {isModalOpen && (
+        <div className="fixed bottom-4 right-4 bg-white p-6 rounded-lg w-80 sm:w-96 max-w-sm mx-auto shadow-lg z-50">
+          {/* Adjusting padding to remove excessive margins */}
+          <div className="bg-white p-4 rounded-lg w-full max-w-xs sm:max-w-sm mx-auto text-left">
+            <h3 className="text-2xl font-bold text-gray-900 text-center mb-4">{userName} Stats</h3>
+
+            <div className="flex justify-between text-black text-md mb-2">
+              <span>Winnings:</span>
+              <span>{winnings}</span>
+            </div>
+            <div className="flex justify-between text-black text-md mb-2">
+              <span>Losses:</span>
+              <span>{losses}</span>
+            </div>
+            <div className="flex justify-between text-black text-md mb-2">
+              <span>Games Played:</span>
+              <span>{gamesPlayed}</span>
+            </div>
+
+            {/* Close Button */}
+            <button
+              className="mt-4 bg-red-500 text-white p-2 rounded-lg"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
