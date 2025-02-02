6f075ac9e4306fbe56fc4d1ff37bc0ce4c645454
Ddan71117 <137979308+Ddan71117@users.noreply.github.com>
2025-02-01 20:40:15 -0700

    NavBar implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
 import Nav from "./components/Nav";
+import NavPostLogin from "./components/NavPostLogin";
 import { robotoMono } from "./fonts";
 import "./globals.css";
+import { usePathname } from "next/navigation";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: LayoutProps) {
+  const pathname = usePathname();
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    setShowNav(
+      pathname === "/" || pathname === "/login" || pathname === "/signup"
+    );
+  }, [pathname]);
+
   return (
     <html lang="en">
       <body
@@ -16,7 +29,7 @@ export default function RootLayout({ children }: LayoutProps) {
           backgroundImage: "url('/images/fade_lrc.jpg')",
         }}
       >
-        <Nav />
+        {showNav ? <Nav /> : <NavPostLogin />}
         {children}
       </body>
     </html>
