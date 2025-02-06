3a85bbf595305630b5f7be0964c1634cad7ae8d9
andrewsoper1 <fluffypuffmalloween@gmail.com>
2025-02-06 15:59:35 -0700

    deleted unused signout route file and db setup code

--- pages/api/setup.ts
+++
@@ -1,9 +0,0 @@
-import { createUsersTable } from "../../app/lib/actions";
-
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Call the function to create the users table
-  await createUsersTable();
-  res.status(200).json({ message: "Table created successfully!" });
-}
